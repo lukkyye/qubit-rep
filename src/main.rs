3654cda5ed7285927ae439b058b20e0fe74fc6c3
lukkyye <lucas.icasetti@fing.edu.uy>
2025-01-19 06:24:41 -0300

    feat(phase_shift): add phase shift functionality feat(measure): add functionality to measure probabilities of states doc(main): update documentation

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 use core::fmt;
-use std::{fmt::{Debug, LowerExp},ops::{Add, Mul}};
+use std::{f32::NAN, fmt::Debug, ops::{Add, Mul, Sub}};
 use rand::{self, Rng};
 #[derive(PartialEq, Clone, Copy)]
 enum Forms{
@@ -21,10 +21,6 @@ impl<T> Vec2x1<T> where T: std::ops::Mul<Output = T> + std::clone::Clone, {
     fn new(a: T, b: T)->Self{
         Vec2x1(a, b)
     }
-    // fn scale(mut self, k: T){
-    //     self.0=self.0*k.clone();
-    //     self.1=self.1*k;
-    // }
 }
 impl<T> Add for Vec2x1<T> where T: Add<Output=T>, {
     type Output = Vec2x1<T>;
@@ -57,6 +53,14 @@ impl Complex {
             rep: form
         }
     }
+
+    /// Returns the norm of the Complex
+    /// 
+    /// # Example
+    /// ```rust
+    /// let obj: Complex = Complex::new(2.0, 3.0, Forms::Bin);
+    /// assert_eq!(obj.norm(), 15);
+    /// ```
     fn norm(&self)->f32{
         if self.rep!=Forms::Exp{
             (self.values.0.powi(2)+self.values.1.powi(2)).sqrt()
@@ -64,11 +68,18 @@ impl Complex {
             self.values.0
         }
     }
+    /// Given k, scales the Complex
+    /// # Example
+    /// ```rust
+    /// let cmplx: Complex = Complex::new(2.0, std::f32::consts::PI/4, Forms::Exp);
+    /// cmplx.scale(-3.0);
+    /// assert_eq!(cmplx.norm(), 6.0);
+    /// ```
     fn scale(&mut self, k: f32){
         if (self.rep==Forms::Bin || self.rep==Forms::Coords){
             self.values.0*=k; self.values.1*=k;
         } else {
-            if k!=1.0{
+            if k!=1.0 && k!=-1.0{
                 let (sign, shift): (f32, f32) = if k<0.0 {
                     (k*(-1.0), std::f32::consts::PI)
                 } else {
@@ -80,18 +91,32 @@ impl Complex {
             self.values.1=self.values.1.rem_euclid(std::f32::consts::PI*2.0);
         }
     }
+    /// Go from one Form of expressing a Complex to another one.
+    /// ### Forms:
+    /// ```rust
+    /// Forms::Bin // -> Binomial notation
+    /// Forms::Exp // -> Exponential notation
+    /// Forms::Coords // -> Cartesian notation
+    /// ```
+    /// # Example
+    /// 
+    /// ```rust
+    /// let complex1: Complex = Complex::new(1.0, 0.0, Forms::Coords);
+    /// complex1.translate_to(Forms::Exp); // (1.0, 0.0) -> 1e^(0i)
+    /// complex1.translate_to(Forms::Bin); // 1.0e^(0.0i) -> 1.0 + 0.0i
+    /// ```
     fn translate_to(&mut self, to: Forms){
         if self.rep!=to{
-            let theta = (self.values.1).atan2(self.values.0);
-            if (self.rep==Forms::Bin || self.rep==Forms::Coords) && to==Forms::Exp{
+            if (to==Forms::Exp){
+                let a=self.values.0;
                 self.values.0=self.norm();
-                self.values.1=theta;
-            } else if self.rep==Forms::Exp && ((Forms::Coords==to) || (Forms::Bin==to)){
-                self.values.1=self.values.0*(theta.cos());
-                self.values.0=self.values.0*(theta.sin());
+                self.values.1=self.values.1.atan2(a);
+            } else {
+                let norm= self.norm();
+                self.values.0=norm*self.values.1.cos();
+                self.values.1=norm*self.values.1.sin();
             }
-            self.rep=to.clone();
-            /*Note that; Bin <-> Coords in both ways are unnecesary, because (a, b)<->a+bi */
+            self.rep=to;
         }
     }
 }
@@ -127,33 +152,75 @@ impl Clone for Complex {
 impl Add for Complex {
     type Output = Complex;
     fn add(self, rhs: Self) -> Self::Output {
-        if self.rep==rhs.rep && (self.rep==Forms::Bin || self.rep==Forms::Coords){
-            Complex::new(self.values.0+rhs.values.0, self.values.1+rhs.values.1, self.rep.clone())
+        if self.rep==rhs.rep {
+            let mut buf1: Complex;
+            let mut buf2: Complex;
+            if self.rep == Forms::Exp {
+                buf1=self; buf1.translate_to(self.rep);
+                buf2=rhs; buf2.translate_to(self.rep);
+            } else {
+                buf1=self;
+                buf2=rhs;
+            }
+            Complex::new(buf1.values.0+buf2.values.0, buf1.values.1+buf2.values.1, self.rep.clone())
         } else {
-            assert!(false, "Cannot Add <Complex> in different forms: <{}> and <{}>", self.rep, rhs.rep);
-            Complex::new(1.0, 1.0, Forms::Bin)
+            assert!(false, "Cannot Sub <Complex> in different forms: <{}> + <{}>", self.rep, rhs.rep);
+            Complex::new(NAN, NAN, Forms::Bin)
         }
+    }
+}
+impl Sub for Complex {
+    type Output = Complex;
+    fn sub(self, rhs: Self) -> Self::Output {
+        let mut pivot = rhs;
+        pivot.scale(-1.0);
+        self.add(pivot)
     }
 }
 impl Mul for Complex {
     type Output = Complex;
     fn mul(self, rhs: Self) -> Self::Output {
-        if (self.rep==Forms::Bin && rhs.rep==Forms::Bin) || (self.rep==Forms::Coords && rhs.rep==Forms::Coords) {
-            Complex::new(self.values.0*rhs.values.1-self.values.1*rhs.values.1, self.values.0*rhs.values.1+self.values.1*rhs.values.0, self.rep)
-        } else if self.rep==Forms::Exp && rhs.rep==Forms::Exp{
-            Complex::new((self.values.0*rhs.values.0).abs(), self.values.1+rhs.values.1, Forms::Exp)
+        if (self.rep==rhs.rep){
+            if self.rep==Forms::Exp{
+                let mut pivot = rhs.clone();
+                pivot.translate_to(Forms::Exp);
+                let shift= if pivot.values.0*self.values.0<0.0 {
+                    std::f32::consts::PI
+                } else {
+                    0.0
+                };
+                Complex::new((pivot.values.0*self.values.0).abs(), pivot.values.1+self.values.1+shift, Forms::Exp)
+            } else {
+                Complex::new(self.values.0*rhs.values.0-rhs.values.1*self.values.1, self.values.0*rhs.values.1+self.values.1*rhs.values.0, self.rep)
+            }
         } else {
-            assert!(false, "Cannot Multiply two Complex in different Forms: {} * {}", self.rep, rhs.rep);
-            Complex::new(1.0, 1.0, Forms::Bin)
+            assert!(false, "Cannot multiply two complex in different Forms: <{}> * <{}>", self.rep, rhs.rep);
+            Complex::new(NAN, NAN, Forms::Bin)
         }
     }
 }
 
+/// Qubit object
 struct Qubit(Vec2x1<Complex>);
 impl Qubit {
+    /// Create a Qubit manually setting by your own risk z and z2 (not recommended)
+    /// If you will use this, make sure that |z1|²+|z2|²=1
+    /// # Example
+    /// ```rust
+    /// let qubit1: Qubit = Qubit::new(Complex::new(-0.32, 0.0, Forms::Bin), Complex::new(0.68, 0.0, Forms::Bin));
+    /// assert_eq!(Qubit.0.0.norm()+Qubit.0.1.norm(), 1.0);
+    /// ```
     fn new(z: Complex, z2: Complex)->Self{
         return Qubit(Vec2x1(z, z2));
     }
+    fn print(&self){
+        println!("{:#?}", self);
+    }
+    /// Initialize a Qubit automatically (random Complex numbers satisfying |z1|+|z2|=1)
+    /// # Example
+    /// ```rust
+    /// let qubit1: Qubit = Qubit::init(Forms::Bin);
+    /// ```
     fn init(form: Forms)->Self {
         let mut a = rand::thread_rng();
         let state1: f32 = a.gen();
@@ -175,6 +242,25 @@ impl Qubit {
         }
         toreturn
     }
+
+    /// Prints the probabilities to measure basis states
+    /// 
+    /// # Example
+    /// ```rust
+    /// let qubit1: Qubit = Qubit::init(Forms::Bin);
+    /// qubit1.measure() // Output looks like: P(|0⟩)= 0.4923013, P(|1⟩)= 0.5076987
+    /// ```
+    fn measure(&self){
+        let prob_0 = self.0.0.norm().powi(2);
+        println!("P(|0⟩)= {}, P(|1⟩)= {}", prob_0, 1.0-prob_0);
+    }
+    /// Collapse Qubit state to one of both basis states.
+    /// # Example
+    /// ```rust
+    /// let qubit1: Qubit = Qubit::init(Forms::Bin);
+    /// qubit1.collapse() 
+    /// println!("{:#?}", qubit1); // Output looks like: |ϕ⟩=(-0.38470358-0.45738223i, 0.49093932+0.6338637i)
+    /// ```
     fn collapse(&self)->Self{
         let mut thrd = rand::thread_rng();
         let decider: f32 = thrd.gen();
@@ -189,18 +275,23 @@ impl Qubit {
             Qubit::new(Complex::new(modifier.0, 0.0, Forms::Bin), Complex::new(modifier.1, 0.0, Forms::Bin))
         }
     }
+    /// Apply Hadamard gate
+    /// 
+    /// # Example
+    /// ```rust
+    /// let mut qubit1: Qubit = Qubit::init(Forms::Bin);
+    /// qubit1.collapse(); // Now, the Qubit collapsed to a basis state
+    /// qubit1.hadarmard(); // Here, the Qubit becomes undetermined
+    /// qubit1.measure(); // Output: 
+    /// 
+    /// ```
     fn hadamard(&mut self){
-        let alpha = self.0.0;
-        let mut beta = self.0.1;
-
-        let k = 1.0/(2.0_f32.sqrt());
-        self.0.0=alpha+beta;
-        beta.scale(-1.0);
-        self.0.1=alpha+beta;
-        let norm = (self.0.0+self.0.1).norm();
-        self.0.0.scale(k*norm);
-        self.0.1.scale(k*norm);
+        let mut buf1 = self.0.0+self.0.1; buf1.scale(1.0/(2.0_f32.sqrt()));
+        let mut buf2 = self.0.0-self.0.1; buf2.scale(1.0/(2.0_f32.sqrt()));
+        self.0.0=buf1;
+        self.0.1=buf2;
     }
+    
     fn px(&mut self){
         let saved = self.0.0;
         self.0.0=self.0.1;
@@ -218,7 +309,7 @@ impl Qubit {
             self.0.1.scale(-1.0);
         }
     }
-    fn phaseshift(&mut self, phi: f32){
+    fn phase_shift(&mut self, phi: f32){
         self.0.1.translate_to(Forms::Exp);
         self.0.1=self.0.1 * Complex::new(1.0, phi, Forms::Exp);
         self.0.1.translate_to(self.0.0.rep);
@@ -235,8 +326,11 @@ impl ToString for Qubit {
     }
 }
 fn main() {
-    let mut cmplx1: Complex = Complex::new(2.0, 3.14, Forms::Exp);
-    cmplx1.scale(-4.0);
-    println!("{:#?}", cmplx1);
-    //Ive to solve translate_to()
+    let mut algo = Qubit::init(Forms::Exp);
+    let mut newqubit1 = algo.collapse();
+    newqubit1.hadamard();
+    newqubit1.measure();
+    newqubit1.hadamard();
+    newqubit1.measure();
+    println!("{} {}", newqubit1.0.0.norm(), newqubit1.0.1.norm());
 }
